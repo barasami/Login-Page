e87693aaf7c1c48b20da852577061d0534097573
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-04-09 21:16:46 +0300

    added mui component

--- src/Loginpg/Login.js
+++ src/Loginpg/Login.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import './Login.css'
 import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
 
 function Login() {
   return (
-    <Paper>
-        <TextField
-          label="Username"
-          id="outlined-size-small"
-          size="small"
-        />
-    </Paper>
+    <div>
+        <div>
+            <TextField
+            label="Username"
+            id="outlined-size-small"
+            size="small"
+            />
+        </div>
+        <div>
+            <TextField
+            label="Password"
+            id="outlined-size-small"
+            size="small"
+            type='password'
+            />
+        </div>
+       <div>
+            <Button variant="contained" color="primary">
+                Login
+            </Button>
+       </div>
+        
+    </div>
   )
 }
 
